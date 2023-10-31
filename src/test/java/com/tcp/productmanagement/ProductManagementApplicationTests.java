a30f1928be922ff6635adf542a0ef92b4d626658
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-10-31 10:53:12 -0700

    first commit

---
+++ src/test/java/com/tcp/productmanagement/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcp.productmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
